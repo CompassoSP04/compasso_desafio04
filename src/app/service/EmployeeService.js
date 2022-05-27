8371fd8452f0190f6930f44ddb08bac091c28f2e
Paulo Rayann <paulorpsena@gmail.com>
2022-05-27 17:02:07 -0300

    feat: create get all Employees method

--- src/app/service/EmployeeService.js
+++ src/app/service/EmployeeService.js
@@ -1,8 +1,19 @@
+const res = require('express/lib/response')
 const EmployeeRepository = require('../repository/EmployeeRepository')
+const Employee = require('../schema/EmployeeSchema')
+
 class EmployeeService {
-    create(payload) {
-        const result = EmployeeRepository.create(payload)
+
+    //Create
+    async create(payload) {
+        const result = await EmployeeRepository.create(payload)
         return result
+    }
+    //List All
+    async list(payload) {
+        const result = await EmployeeRepository.list(payload)
+        return result
+        
     }
 }
 module.exports = new EmployeeService()
