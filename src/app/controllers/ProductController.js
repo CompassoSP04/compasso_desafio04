e27be885d948550060a9f2072717dd6467680a76
Paulo Rayann <86445602+paulorayann@users.noreply.github.com>
2022-05-29 18:41:58 -0300

    Revert "Add custom error messages "

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -7,23 +7,14 @@ class ProductController {
             // const result = await ProductService.create(req.body)
             return res.status(201).json(result)
         } catch (error) {
-            return res.status(400).json({
-                message: "Bad Request",
-                details: [{
-                  message: error.message
-                }]
-              })
+            return res.status(400).json(error)
+
         }
     }
     //List All   
     async list(req, res) {
-        try {
         const result = await ProductService.list(req.query)
         return res.status(200).json(result)
-        } catch (error) {
-            return res.status(400)
-        }
-        
     }
 
     
